c3c4da8fa48034f8e8485b7bda4d82b373897751
Koukunn_ <118247906+gamelist1990@users.noreply.github.com>
2026-09-18 09:16:54 +0900

    Add Protect audit model

---
+++ src/main/java/org/pexserver/koukunn/bettersurvival/Modules/Feature/Protect/ProtectAction.java
@@ -0,0 +1,12 @@
+package org.pexserver.koukunn.bettersurvival.Modules.Feature.Protect;
+
+/**
+ * Protect が永続化する操作種別。
+ */
+public enum ProtectAction {
+    BLOCK_BREAK,
+    BLOCK_PLACE,
+    CONTAINER_OPEN,
+    CONTAINER_CLOSE,
+    CONTAINER_CHANGE
+}
